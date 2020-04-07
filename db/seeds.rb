e4e1ea7dbe0e62d50db7d47a12399825bdfa55c0
Premti Krasniqi <premti@gmail.com>
2020-04-07 16:25:17 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+15.times do 
+    Coupon.create(coupon_code: Faker::IDNumber.spanish_foreign_citizen_number , store: Faker::FunnyName.name)
+
+end
